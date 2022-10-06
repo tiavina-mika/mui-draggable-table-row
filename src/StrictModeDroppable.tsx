0df6c01e41fffecd072a89d40fc2d23a7160756b
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2022-10-06 10:06:20 +0300

    comment code

--- src/StrictModeDroppable.tsx
+++ src/StrictModeDroppable.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { Droppable, DroppableProps } from "react-beautiful-dnd";
 
+/**
+ * react-beautiful-dnd does not work with the React 18 Strict mode
+ * this is a workaround from https://github.com/atlassian/react-beautiful-dnd/issues/2399
+ */
 export const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
   const [enabled, setEnabled] = useState(false);
 
